7c8ddeeb8458768eb70677ba723e6495a5ec38d7
Mister X <martinschulz2000@gmail.com>
2017-12-08 16:02:41 +0100

    changed assembly info

--- ScreenMonitor/Properties/AssemblyInfo.cs
+++ ScreenMonitor/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ScreenMonitor")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Mister X")]
 [assembly: AssemblyProduct("ScreenMonitor")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
