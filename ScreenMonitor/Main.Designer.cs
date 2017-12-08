cd7f473d1c3836d8a7dfdc55332a97409e740ae8
Mister X <martinschulz2000@gmail.com>
2017-12-08 15:11:37 +0100

    first commit

---
+++ ScreenMonitor/Main.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace ScreenMonitor
+{
+    partial class frm_main
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbx_main = new System.Windows.Forms.PictureBox();
+            this.tmr_screen = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_main)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbx_main
+            // 
+            this.pbx_main.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbx_main.Location = new System.Drawing.Point(0, 0);
+            this.pbx_main.Name = "pbx_main";
+            this.pbx_main.Size = new System.Drawing.Size(284, 261);
+            this.pbx_main.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbx_main.TabIndex = 0;
+            this.pbx_main.TabStop = false;
+            // 
+            // tmr_screen
+            // 
+            this.tmr_screen.Enabled = true;
+            this.tmr_screen.Interval = 10;
+            this.tmr_screen.Tick += new System.EventHandler(this.tmr_screen_Tick);
+            // 
+            // frm_main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.pbx_main);
+            this.Name = "frm_main";
+            this.Text = "ScreenMonitor";
+            this.Load += new System.EventHandler(this.frm_main_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frm_main_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_main)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbx_main;
+        private System.Windows.Forms.Timer tmr_screen;
+    }
+}
+
---
+++ ScreenMonitor/Main.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScreenMonitor
+{
+    public partial class frm_main : Form
+    {
+        public frm_main()
+        {
+            InitializeComponent();
+        }
+
+        private int screenInd;
+        private int screenScale = 4;
+
+        private void frm_main_Load(object sender, EventArgs e)
+        {
+            ChangeWindow();
+        }
+
+        private void tmr_screen_Tick(object sender, EventArgs e)
+        {
+            pbx_main.Image = GetScreen(Screen.AllScreens[screenInd]);
+            ChangeWindow();
+            GC.Collect();
+        }
+
+        private static Bitmap GetScreen(Screen screen)
+        {
+            var screenshot = new Bitmap(screen.Bounds.Width,
+                screen.Bounds.Height);
+            // Create a graphics object from the bitmap
+            var gfxScreenshot = Graphics.FromImage(screenshot);
+            // Take the screenshot from the upper left corner to the right bottom corner
+            gfxScreenshot.CopyFromScreen(
+                screen.Bounds.X, screen.Bounds.Y,
+                0, 0,
+                screen.Bounds.Size);
+
+            return screenshot;
+        }
+
+        private void ChangeWindow()
+        {
+            var s = Screen.AllScreens[screenInd].Bounds.Size;
+            this.ClientSize = new Size(s.Width / screenScale, s.Height / screenScale);
+        }
+
+        private void frm_main_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Right:
+                    if (screenInd < Screen.AllScreens.Length - 1)
+                        screenInd += 1;
+                    break;
+                case Keys.Left:
+                    if (screenInd > 0)
+                        screenInd -= 1;
+                    break;
+                case Keys.Up:
+                    if (screenScale > 1)
+                    {
+                        screenScale -= 1;
+                        ChangeWindow();
+                    }
+                    break;
+                case Keys.Down:
+                    if (screenScale < 10)
+                    {
+                        screenScale += 1;
+                        ChangeWindow();
+                    }
+                    break;
+            }
+        }
+    }
+}
